aa3df20102f58ba9f9015f858ccc7e563fa765bb
yujin9619 <yujin9588@gmail.com>
2021-09-26 16:27:59 +0900

    ServerUtil에 로그인 수행 함수 작업  - 호출까지는 작업 완

--- app/src/main/java/com/yujin/colosseum_210903/utils/ServerUtil.kt
+++ app/src/main/java/com/yujin/colosseum_210903/utils/ServerUtil.kt
@@ -1,8 +1,35 @@
 package com.yujin.colosseum_210903.utils
 
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
 class ServerUtil  {
 
     companion object {
+
+       private val HOST_URL = "http://54.180.52.26"
+
+        fun postRequestSignIn( id : String, pw : String) {
+
+            val urlString = "${HOST_URL}/user"
+
+            val formData = FormBody.Builder()
+                .add("email", id)
+                .add("password", pw)
+                .build()
+
+            val request = Request.Builder()
+                .url(urlString)
+                .post(formData)
+                .build()
+
+            val client = OkHttpClient()
+
+            client.newCall(request)
+
+
+        }
 
 
     }
